4e2de1d4531c5c5ef2591ff904137e788e5d7533
Artem Balandin <artem_balandin@outlook.com>
2022-07-23 21:14:33 +0300

    add babel to webpack

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -8,8 +8,8 @@ const js = () => {
 				app.plugins.notify.onError({
 					title: 'JS',
 					message: 'Error: <%= error.message %> ',
-				})
-			)
+				}),
+			),
 		)
 		.pipe(
 			webpack({
@@ -18,7 +18,21 @@ const js = () => {
 				output: {
 					filename: 'main.min.js',
 				},
-			})
+				module: {
+					rules: [
+						{
+							test: /\.m?js$/,
+							exclude: /(node_modules|bower_components)/,
+							use: {
+								loader: 'babel-loader',
+								options: {
+									presets: ['@babel/preset-env'],
+								},
+							},
+						},
+					],
+				},
+			}),
 		)
 		.pipe(app.gulp.dest(app.path.build.js))
 		.pipe(app.gulp.dest(app.path.buildLocalServer.js))
